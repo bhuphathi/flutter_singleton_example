ede0963930ff2ce6c16fedb641fe3de18499cb44
bhuphathi <bhuphathi@gmail.com>
2023-09-28 08:29:39 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Singleton Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.green[200],
+        body: const Center(
+          child: Column(
+            children: [
+              Text('Singleton Demo Home Page'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
